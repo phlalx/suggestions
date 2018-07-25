4e044851f67a9a04323b393979a33ba4b0c2415b
phlalx <phlalx@users.noreply.github.com>
2018-07-25 22:13:57 +0200

    put db connection in app context

--- service.py
+++ service.py
@@ -3,33 +3,29 @@ import redis
 
 app = flask.Flask(__name__, static_url_path='')
 
+from flask import g
+
+def get_db():
+    if 'db' not in g:
+        g.db = redis.Redis(host="localhost", db=0, socket_connect_timeout=2, socket_timeout=2)
+    return g.db
+
 @app.route('/getkey')
 def getkey():
     # TODO set up connection somewhere else
     key = flask.request.args.get('key', default = '', type = str)
-    res = None
-    try:
-        red = redis.Redis(host="localhost", db=0, socket_connect_timeout=2, socket_timeout=2)
-        res = red.get(key)
-    except Exception as e:
-        print("cant't access redis", e)
-        pass
+    red = get_db()
+    res = red.get(key)
     if res is None:
-        res = "unkown"
+        res = 'unknown'
     return res
 
 @app.route('/setkeyvalue')
 def setkeyvalue():
     key = flask.request.args.get('key', default = '', type = str)
     value = flask.request.args.get('value', default = '', type = str)
-    # TODO set up connection somewhere else
-    try:
-        red = redis.Redis(host="localhost", db=0, socket_connect_timeout=2, socket_timeout=2)
-        red.set(key, value)
-    except Exception as e:
-        print("cant't access redis", e)
-        pass
-
+    red = get_db()
+    red.set(key, value)
     return ""
 
 if __name__ == '__main__':
