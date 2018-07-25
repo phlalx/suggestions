8dd69391c604c9777b21d3a7c586447489d3fed8
phlalx <phlalx@users.noreply.github.com>
2018-07-25 21:28:26 +0200

    Got rid of prefix tree construction

--- service.py
+++ service.py
@@ -1,65 +1,7 @@
 import flask
-import tree
 import redis
 
-query_tree = tree.build_tree('words.txt')
-
-def counter():
-    i = 0
-    while True: 
-        yield str(i)
-        i += 1
-
-def copy_tree_to_redis(tree):
-
-    red = redis.Redis(host="localhost", db=0, socket_connect_timeout=2, socket_timeout=2)
-    node_id_gen = counter()
-
-    def fill_tree_redis(tree):
-
-        node_id = next(node_id_gen)
-        print(node_id, tree.suggestions.all(), tree.children)
-
-        redis_value = {}
-        redis_value['suggestions'] = " ".join(tree.suggestions.all())
-        for letter, node in tree.children.items():
-            redis_value[letter] = fill_tree_redis(node)
-
-        red.hmset(node_id, redis_value)
-
-        return node_id
-        
-    fill_tree_redis(tree)
-
 app = flask.Flask(__name__, static_url_path='')
-
-# copy_tree_to_redis(query_tree)
-
-# res = red.hget(key, 'suggestions')
-
-@app.route('/suggestions')
-def suggestions():
-    query = flask.request.args.get('query', default = '', type = str)
-    red = redis.Redis(host="localhost", db=0, socket_connect_timeout=2, socket_timeout=2)
-    tree = "0"
-    res = ""
-
-    for c in query:
-        try:
-            node = red.hget(tree, c)
-        except Exception as e:
-            print('redis exception', e)
-            break
-
-        if res is None:
-            break
-            
-        tree = node
-    
-    if tree is not None:
-        res = red.hget(tree, 'suggestions')
-
-    return res
 
 @app.route('/getkey')
 def getkey():
